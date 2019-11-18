55ddef68ce558552e72e1b67e1405b6728d15aff
ckopecky <kopecky12112@gmail.com>
2019-11-17 19:31:44 -0800

    Change App-js to Dynamic Component

--- src/App.js
+++ src/App.js
@@ -1,92 +1,34 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
-import hash from "./hash";
+import withConditionalRender from "./withConditional";
 import Player from "./Player";
+import NotLoggedIn from "./NotLoggedIn.js";
 import "./App.css";
+
+const DynamicComp = withConditionalRender(Player)(NotLoggedIn);
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      token: null,
-      item: {
-        album: {
-          images: [{ url: "" }]
-        },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms:0,
-      },
-      is_playing: "Paused",
-      progress_ms: 0
+      loading: false,
+      loggedIn: false
     };
   }
-  componentDidMount() {
-    // Set token
-
-    let _token = hash.access_token;
-    if (_token) {
-      // Set token
-      axios.defaults.headers['Authorization'] = "Bearer " + _token;
-      localStorage.setItem("spotify", _token);
-      this.getCurrentPlaylists(_token);
-      }
+  
+  toggle = event => {
+    if(localStorage.getItem('spotify')) {
+    localStorage.removeItem('spotify');
+    this.setState({ loggedIn: false });
+    } else {
+    localStorage.setItem('spotify', true);
+    this.setState({ loggedIn: true });
     }
-
-    getEmbeddedLink = (type, id) => {
-      let startURL = 'https://open.spotify.com/embed/';
-      let URL = `${startURL}${type}/${id}`;
-      console.log(URL)
-      return URL;
-    }
-
-    getCurrentPlaylists = (token) => {
-      axios.get("https://api.spotify.com/v1/me/playlists")
-      .then(response => {
-        let type = response.data.items[2].type;
-        let id = response.data.items[2].id;
-        let url = this.getEmbeddedLink(type, id);
-        this.setState({
-          data: response.data, 
-          items: response.data.items,
-          url, token
-        });
-      })
-      .catch(err => {
-        console.info(err);
-      });    
-    }
+}
   
   render() {
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          { !this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                "%20"
-              )}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-            </a>
-          )}
-          
-           {localStorage.getItem('spotify') && (
-            <Player
-              data={this.state.data}
-              items={this.state.items}
-              url={this.state.url}
-
-            />
-          )}
-
-            {/* <Checkbox /> */}
-        </header>
-      </div>
-    );
+      return (
+        <DynamicComp toggle={this.toggle} />
+      )
   }
 }
 
