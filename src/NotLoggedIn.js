ba62a212b487d11bc2a6cefc71eefd19f9b6f2aa
ckopecky <kopecky12112@gmail.com>
2019-11-17 20:03:59 -0800

    Change to env vars

--- src/NotLoggedIn.js
+++ src/NotLoggedIn.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
 
 
 const NotLoggedIn = () => {
+  console.log(process.env.REACT_APP_SCOPES)
   return (
     <a
       className="btn btn--loginApp-link"
-      href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-        "%20"
-      )}&response_type=token&show_dialog=true`}
+      href={`${process.env.REACT_APP_AUTHENDPOINT}?client_id=${process.env.REACT_APP_CLIENTID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=${process.env.REACT_APP_SCOPES}&response_type=token&show_dialog=true`}
     >
       Login to Spotify
     </a>
