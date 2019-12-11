1e35a94d7ffff73905346b75d734b7f49f72e394
ckopecky <kopecky12112@gmail.com>
2019-12-11 08:39:51 -0800

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import withConditionalRender from "./withConditional";
 import Player from "./components/Player/Player";
-import NotLoggedIn from ".components/NotLoggedIn.js";
+import NotLoggedIn from "./components/NotLoggedIn.js";
 import "./App.css";
 
 const DynamicComp = withConditionalRender(Player)(NotLoggedIn);
