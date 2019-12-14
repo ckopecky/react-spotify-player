32e7135c597aa0e511664ab5d8f5d76c25e92e50
ckopecky <kopecky12112@gmail.com>
2019-12-13 17:31:48 -0800

    correct env

--- src/withConditional.js
+++ src/withConditional.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-const currUser = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_CURR_USER : process.env.REACT_APP_DEV_CURR_USER;
+const currUser = process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_PROD_CURR_USER : process.env.REACT_APP_DEV_CURR_USER;
 
 const logOutEndpoint = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_LOGOUT : process.env.REACT_APP_DEV_LOGOUT;
 
