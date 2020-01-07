e8f948019269a84a312ef83263d79c56fcf6b4f1
ckopecky <kopecky12112@gmail.com>
2020-01-07 15:06:02 -0800

    Add basic account settings page

--- src/components/AccountSettings/AccountSettings.js
+++ src/components/AccountSettings/AccountSettings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import "./AccountSettings.css"
 class AccountSettings extends Component {
     constructor(props) {
         super(props);
@@ -8,9 +8,12 @@ class AccountSettings extends Component {
         }
     }
     render() {
+        console.log(this.props.currentUser)
         return (
-            <div>
-                account settings
+            <div className="profile-settings">
+                <div className="text-container">
+                    <h5>To change account settings, please see the accounts overview section in your <span><a rel="noopener noreferrer" target="_blank" href="https://www.spotify.com/us/account/overview/">Spotify</a></span> account</h5>
+                </div>
             </div>
         );
     }
