efa5f4c835da7a352c8b5f61be9e8dc953fee6a5
ckopecky <kopecky12112@gmail.com>
2020-01-07 12:45:11 -0800

    Skeleton page TODO: Marketing page

--- src/components/NotLoggedIn/NotLoggedIn.js
+++ src/components/NotLoggedIn/NotLoggedIn.js
@@ -1,15 +1,8 @@
 import React from "react";
 
-const endpoint = process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_PROD_AUTHENDPOINT : process.env.REACT_APP_DEV_AUTHENDPOINT
-
 const NotLoggedIn = () => {
   return (
-    <a
-      className="btn btn--loginApp-link"
-      href={`${endpoint}/auth/spotify`}
-    >
-      Login to Spotify
-    </a>
+    <div>Not logged in</div>
   );
 };
 
