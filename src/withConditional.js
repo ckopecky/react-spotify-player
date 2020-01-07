d48134442cb4f1cfed04f6707b500894dfa996e3
ckopecky <kopecky12112@gmail.com>
2020-01-07 12:43:43 -0800

    Move logic to App.js

--- src/withConditional.js
+++ src/withConditional.js
@@ -1,48 +1,15 @@
 import React from 'react';
-import axios from 'axios';
-
-const currUser = process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_PROD_CURR_USER : process.env.REACT_APP_DEV_CURR_USER;
-
-const logOutEndpoint = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_LOGOUT : process.env.REACT_APP_DEV_LOGOUT;
 
 const withConditionalRender = FirstComp => SecondComp => 
     class WithConditionalRender extends React.Component {
-        state = {
-            loggedIn: false,
-            currentUser: null
-        }
-
-        handleLogOut = (e) => {
-            axios.get(logOutEndpoint)
-            .then(response => {
-                this.setState({loggedIn: false, currentUser: null});
-                axios.defaults.headers['Authorization'] = null;
-            })
-        }
         
-        componentDidMount = async () => {
-            const res = axios.get(currUser, {withCredentials: true})
-            res.then(response => {
-                if(response.data._id) {
-                    this.setState({loggedIn: !this.state.loggedIn, currentUser: response.data}, () => console.log(this.state));
-                    
-
-                }
-            })
-            .catch(err => {
-                console.log(err.message)
-            })
-
-           
-        }
+        
         
         render() {
-            if(this.state.loggedIn) {
-                console.log("logged in")
-                return <FirstComp {...this.props} loggedIn={this.state.loggedIn} handleClick={this.handleLogOut} currUser={this.state.currentUser}/>;
+            if(this.props.loggedIn) {
+                return <FirstComp {...this.props} loggedIn={this.props.loggedIn} handleClick={this.props.handleLogOut} currentUser={this.props.currentUser}/>;
             } else {
-                console.log("logged out")
-                return <SecondComp {...this.props}/>;
+                return <SecondComp />;
             }
         }
     };
